4bdeae92eeb5e4d774470fd60c85b923159b5114
Angel Montoya <j.gustavo.m245@gmail.com>
2022-07-17 18:15:14 -0500

    pagina de inicio

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\inicio.css">
+    <title>wikicar</title>
+</head>
+<body>
+    <h1>"Don't dream it, just drive it"</h1>
+    <form action="index.php" class="form-box">
+        <button type="submit">
+            Login
+        </button>
+    </form>
+</body>
+</html>
