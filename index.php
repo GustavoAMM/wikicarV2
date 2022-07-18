46adf51af8d06163c11ebff64aecc333ff814c88
Angel Montoya <j.gustavo.m245@gmail.com>
2022-07-17 20:40:31 -0500

    Segunda version del login

--- index.php
+++ index.php
@@ -8,11 +8,15 @@
     <title>wikicar</title>
 </head>
 <body>
-    <h1>"Don't dream it, just drive it"</h1>
-    <form action="index.php" class="form-box">
-        <button type="submit">
-            Login
-        </button>
-    </form>
+    <section id="fondo">
+        <div class="izquierdo"></div>
+        <div class="derecho">
+            <div class="login">
+            <h1><span class="wiki">Wiki</span><span class="car">car</span></h1>
+            <p><span class="uno">"Don't dream it,</span>   <span class="dos">just drive it"</span></p>
+            <button>Entrar</button>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
